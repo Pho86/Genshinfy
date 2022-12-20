d090f2edc673dfb2fba66b55f921aeb2c11ce22d
pho86@my.bcit.ca <pho86@my.bcit.ca>
2022-12-20 15:53:32 -0800

    lorem base styling

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+   <slot></slot>
+</template>
+ 
+
+
+<script>
+export default {
+   head() {
+        return {
+            bodyAttrs: {
+                class: 'hallo'
+            }
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default {
    modules: ['@nuxtjs/tailwindcss'],
-})
-
-
+   buildModules:['@nuxtjs/fontawesome'],
+   head: {
+      link: [
+         {
+            rel: 'stylesheet',
+            href: 'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+            integrity: "sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU",
+            crossorigin: "anonymous"
+         },
+         {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Roboto'
+         }
+      ]
+   }
+}
