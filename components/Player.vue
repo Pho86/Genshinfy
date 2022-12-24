efd0da0cc4aa3dd23628c7f242b1abedbdc18365
pho86@my.bcit.ca <pho86@my.bcit.ca>
2022-12-23 16:15:23 -0800

    audio player dynamic text/position

--- components/Player.vue
+++ components/Player.vue
@@ -2,28 +2,29 @@
    <!-- Player -->
    <div class="fixed bottom-0 left-0 bg-white px-4 py-2 w-full">
       <!-- Track Info -->
-      <div class="text-center">
-         <span class="song-title font-bold">Song Title</span> by
-         <span class="song-artist">Artist</span>
+      <div class="text-center" v-if="current_song.modified_name">
+         <span class="song-title font-bold">{{ current_song.modified_name }}</span> by
+         <span class="song-artist">{{ current_song.display_name }}</span>
       </div>
       <div class="flex flex-nowrap gap-4 items-center">
          <!-- Play/Pause Button -->
          <button type="button" @click.prevent="toggleAudio">
-            <i class="fa  text-gray-500 text-xl" :class="{ 'fa-play' : !playing, 'fa-pause' : playing }"></i>
+            <i class="fa  text-gray-500 text-xl" :class="{ 'fa-play': !playing, 'fa-pause': playing }"></i>
          </button>
          <!-- Current Position -->
          <div class="player-currenttime">{{ seek }}</div>
          <!-- Scrub Container  -->
          <div class="w-full h-2 rounded bg-gray-200 relative cursor-pointer">
             <!-- Player Ball -->
-            <span class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg" style="left: 50%">
+            <span class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg" :style="{ left: playerProgress }">
                <i class="fas fa-circle"></i>
             </span>
             <!-- Player Progress Bar-->
-            <span class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400" style="width: 50%"></span>
+            <span class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
+               :style="{ width: playerProgress }"></span>
          </div>
          <!-- Duration -->
-         <div class="player-duration">{{  duration }}</div>
+         <div class="player-duration">{{ duration }}</div>
       </div>
    </div>
 </template>
@@ -34,12 +35,12 @@ import usePlayerStore from "@/stores/player";
 
 export default {
    name: "Player",
-   methods:{
+   methods: {
       ...mapActions(usePlayerStore, ["toggleAudio"]),
-      
-   }, 
+
+   },
    computed: {
-      ...mapState(usePlayerStore, ["playing", "duration", "seek"])
+      ...mapState(usePlayerStore, ["playing", "duration", "seek", "playerProgress", "current_song"])
    }
 }
 </script>
--- stores/player.js
+++ stores/player.js
@@ -5,10 +5,15 @@ export default defineStore("player", {
       current_song: {},
       sound: {},
       seek: "00:00",
-      duration: "00:00"
+      duration: "00:00",
+      playerProgress: "0%",
    }),
    actions: {
       async newSong(song) {
+         if(this.sound instanceof Howl) {
+            this.sound.unload();
+         }
+
          this.current_song = song;
 
          this.sound = new Howl({
@@ -37,6 +42,7 @@ export default defineStore("player", {
          this.seek = helper.formatTime(this.sound.seek());
          this.duration = helper.formatTime(this.sound.duration());
 
+         this.playerProgress = `${(this.sound.seek() / this.sound.duration() * 100)}%`
          if (this.sound.playing()) {
             requestAnimationFrame(this.progress);
          }
