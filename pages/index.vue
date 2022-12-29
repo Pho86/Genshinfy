de72a4abcf5c34068881e8464438ae361f827094
pho86@my.bcit.ca <pho86@my.bcit.ca>
2022-12-28 16:52:26 -0800

    get most favourited songs

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,12 @@
         <div class="text-white main-header-content">
           <h1 class="font-bold text-5xl mb-5">Listen to Genshinfy Music!</h1>
           <p class="w-11/12 md:w-10/12 mx-auto">
-            Genshinfy is a music streaming platform that allows users to easily listen to music mainly from the game Genshin Impact. It offers a wide selection of songs from the game, making it easy for fans to enjoy their favourite music. <br><br> Whether you want to relax and listen to the game's soundtrack or get pumped up for battle, Genshinfy has you covered with its extensive collection of music. Whether you're a casual player or a hardcore fan, Genshinfy is a place for you to listen to music. (Any type or genre of music is fine to be uploaded.)
+            Genshinfy is a music streaming platform that allows users to easily listen to music. It offers a wide
+            selection of songs from the game, making it easy for fans to enjoy their
+            favourite music. <br><br> Whether you want to relax and listen to the game's soundtrack or get pumped up for
+            battle, Genshinfy has you covered with its extensive collection of music. Whether you're a casual player or
+            a hardcore fan, Genshinfy is a place for you to listen to music. (Any type or genre of music is fine to be
+            uploaded.)
           </p>
         </div>
       </div>
@@ -22,18 +27,31 @@
         class="bg-white rounded border border-gray-200 bg-white-500 relative flex flex-col dark:bg-gray-700 dark:border-gray-500">
         <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200 dark:border-gray-500"
           v-icon.right="'headphones-alt'">
-          <span class="card-title dark:text-gray-100">Recommended Songs</span>
+          <span class="card-title dark:text-gray-100">Most Favourited Songs</span>
         </div>
+        <ol class="flex flex-row justify-center gap-5">
+          <song-card v-for="song in randomSongs" :key="song.docID" :song="song" />
+        </ol>
       </div>
     </section> -->
 
     <!-- Main Content -->
-    <section class="container mx-auto">
+    <section class="container mx-auto mt-10">
       <div
         class="bg-white rounded border border-gray-200 bg-white-500 relative flex flex-col dark:bg-gray-700 dark:border-gray-500 transition duration-500">
         <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200 dark:border-gray-500"
           v-icon.right="'headphones-alt'">
           <span class="card-title dark:text-gray-100">Songs</span>
+          <!-- <div class="font-normal dark:text-gray-100 flex items-center mt-4 ">Sort By:
+            <select v-model="sort"
+              class="ml-2 block py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded">
+              <option value="name(a-z)">Name(A-Z)</option>
+              <option value="name(z-a)">Name(Z-A)</option>
+              <option value="favourites">Favourites</option>
+            </select>
+          </div> -->
+
+
         </div>
         <!-- Playlist -->
         <ol id="playlist">
@@ -60,13 +78,16 @@ export default {
       songs: [],
       maxPerPage: 10,
       pendingRequest: false,
+      sort: "name",
+      randomSongs: [],
     }
   },
   components: {
-    AppSongItem
+    AppSongItem,
   },
   async created() {
     await this.getSongs();
+    await this.getPopularSongs();
     if (process.client) {
       window.addEventListener("scroll", this.handleScroll)
     }
@@ -98,12 +119,12 @@ export default {
       if (this.songs.length) {
         const lastDocRef = doc(db, "songs", (this.songs[this.songs.length - 1].docID))
         const lastDoc = await getDoc(lastDocRef)
-        const songCollection = await query(collection(db, "songs"), limit(this.maxPerPage), orderBy('modified_name'), startAfter(lastDoc));
+        const songCollection = await query(collection(db, "songs"), limit(this.maxPerPage), orderBy('modified_name', "asc"), startAfter(lastDoc));
         querySnapshot = await getDocs(songCollection);
       }
 
       else {
-        const songCollection = await query(collection(db, "songs"), limit(this.maxPerPage), orderBy('modified_name'));
+        const songCollection = await query(collection(db, "songs"), limit(this.maxPerPage), orderBy('modified_name', "asc"));
         querySnapshot = await getDocs(songCollection);
       }
       querySnapshot.forEach((doc) => {
@@ -113,6 +134,17 @@ export default {
         })
       })
       this.pendingRequest = false;
+    },
+    async getPopularSongs() {
+      const db = firebase().db;
+      const songCollection = await query(collection(db, "songs"), limit(3), orderBy("favourited", "desc"));
+      const querySnapshot = await getDocs(songCollection);
+      querySnapshot.forEach((doc) => {
+        this.randomSongs.push({
+          ...doc.data(),
+          docID: doc.id,
+        })
+      })
     }
   },
 
